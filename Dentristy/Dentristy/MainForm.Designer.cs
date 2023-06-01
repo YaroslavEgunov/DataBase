19b606de107a70b772ead80ac772c581b906d005
YaroslavEgunov517-1 <99376130+YaroslavEgunov517-1@users.noreply.github.com>
2023-06-01 18:15:37 +0700

    Пара исправлений по 2-ой лабе

--- Dentristy/Dentristy/MainForm.Designer.cs
+++ Dentristy/Dentristy/MainForm.Designer.cs
@@ -31,19 +31,23 @@
             this.components = new System.ComponentModel.Container();
             this.menuStripMain = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справочникToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMain = new System.Windows.Forms.ToolStrip();
+            this.выходToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.справочникToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.процедурыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.сотрудникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.медицинскиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.выходToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.оПрограммеToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMain = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.справочникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.процедурыToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.врачиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.медицинскиеКартыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStripMain.SuspendLayout();
             this.contextMenuStripMain.SuspendLayout();
             this.toolStripMain.SuspendLayout();
@@ -69,6 +73,81 @@
             this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
+            // справочникToolStripMenuItem
+            // 
+            this.справочникToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.процедурыToolStripMenuItem,
+            this.сотрудникиToolStripMenuItem,
+            this.медицинскиToolStripMenuItem});
+            this.справочникToolStripMenuItem.Name = "справочникToolStripMenuItem";
+            this.справочникToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.P)));
+            this.справочникToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.справочникToolStripMenuItem.Text = "Справочники";
+            // 
+            // contextMenuStripMain
+            // 
+            this.contextMenuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.справочникиToolStripMenuItem});
+            this.contextMenuStripMain.Name = "contextMenuStripMain";
+            this.contextMenuStripMain.Size = new System.Drawing.Size(150, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem1,
+            this.оПрограммеToolStripMenuItem1});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "Файл";
+            // 
+            // toolStripMain
+            // 
+            this.toolStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStripMain.Location = new System.Drawing.Point(0, 24);
+            this.toolStripMain.Name = "toolStripMain";
+            this.toolStripMain.Size = new System.Drawing.Size(800, 25);
+            this.toolStripMain.TabIndex = 2;
+            this.toolStripMain.Text = "toolStrip1";
+            // 
+            // выходToolStripMenuItem1
+            // 
+            this.выходToolStripMenuItem1.Image = global::Dentistry.Properties.Resources.Exit_50x501;
+            this.выходToolStripMenuItem1.Name = "выходToolStripMenuItem1";
+            this.выходToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem1.Text = "Выход";
+            this.выходToolStripMenuItem1.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem1
+            // 
+            this.оПрограммеToolStripMenuItem1.Image = global::Dentistry.Properties.Resources.About_50x50;
+            this.оПрограммеToolStripMenuItem1.Name = "оПрограммеToolStripMenuItem1";
+            this.оПрограммеToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.оПрограммеToolStripMenuItem1.Text = "О программе...";
+            this.оПрограммеToolStripMenuItem1.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::Dentistry.Properties.Resources.Exit_50x501;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::Dentistry.Properties.Resources.About_50x50;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
             // выходToolStripMenuItem
             // 
             this.выходToolStripMenuItem.Image = global::Dentistry.Properties.Resources.Exit_50x501;
@@ -86,17 +165,6 @@
             this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
             this.оПрограммеToolStripMenuItem.Text = "О программе...";
             this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
-            // 
-            // справочникToolStripMenuItem
-            // 
-            this.справочникToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.процедурыToolStripMenuItem,
-            this.сотрудникиToolStripMenuItem,
-            this.медицинскиToolStripMenuItem});
-            this.справочникToolStripMenuItem.Name = "справочникToolStripMenuItem";
-            this.справочникToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.P)));
-            this.справочникToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
-            this.справочникToolStripMenuItem.Text = "Справочники";
             // 
             // процедурыToolStripMenuItem
             // 
@@ -118,74 +186,46 @@
             // 
             // медицинскиToolStripMenuItem
             // 
+            this.медицинскиToolStripMenuItem.Image = global::Dentistry.Properties.Resources.MedCard_50x50;
             this.медицинскиToolStripMenuItem.Name = "медицинскиToolStripMenuItem";
             this.медицинскиToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.M)));
             this.медицинскиToolStripMenuItem.Size = new System.Drawing.Size(227, 22);
             this.медицинскиToolStripMenuItem.Text = "Медицинские карты";
             this.медицинскиToolStripMenuItem.Click += new System.EventHandler(this.медицинскиToolStripMenuItem_Click);
             // 
-            // contextMenuStripMain
+            // справочникиToolStripMenuItem
             // 
-            this.contextMenuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1});
-            this.contextMenuStripMain.Name = "contextMenuStripMain";
-            this.contextMenuStripMain.Size = new System.Drawing.Size(104, 26);
+            this.справочникиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.процедурыToolStripMenuItem1,
+            this.врачиToolStripMenuItem,
+            this.медицинскиеКартыToolStripMenuItem});
+            this.справочникиToolStripMenuItem.Name = "справочникиToolStripMenuItem";
+            this.справочникиToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.справочникиToolStripMenuItem.Text = "Справочники";
             // 
-            // toolStripMenuItem1
+            // процедурыToolStripMenuItem1
             // 
-            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.выходToolStripMenuItem1,
-            this.оПрограммеToolStripMenuItem1});
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
-            this.toolStripMenuItem1.Text = "Файл";
+            this.процедурыToolStripMenuItem1.Image = global::Dentistry.Properties.Resources.Procedures_50x50;
+            this.процедурыToolStripMenuItem1.Name = "процедурыToolStripMenuItem1";
+            this.процедурыToolStripMenuItem1.Size = new System.Drawing.Size(186, 22);
+            this.процедурыToolStripMenuItem1.Text = "Процедуры";
+            this.процедурыToolStripMenuItem1.Click += new System.EventHandler(this.процедурыToolStripMenuItem_Click);
             // 
-            // выходToolStripMenuItem1
+            // врачиToolStripMenuItem
             // 
-            this.выходToolStripMenuItem1.Image = global::Dentistry.Properties.Resources.Exit_50x501;
-            this.выходToolStripMenuItem1.Name = "выходToolStripMenuItem1";
-            this.выходToolStripMenuItem1.Size = new System.Drawing.Size(158, 22);
-            this.выходToolStripMenuItem1.Text = "Выход";
-            this.выходToolStripMenuItem1.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            this.врачиToolStripMenuItem.Image = global::Dentistry.Properties.Resources.Doctor_50x50;
+            this.врачиToolStripMenuItem.Name = "врачиToolStripMenuItem";
+            this.врачиToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.врачиToolStripMenuItem.Text = "Сотрудники";
+            this.врачиToolStripMenuItem.Click += new System.EventHandler(this.сотрудникиToolStripMenuItem_Click);
             // 
-            // оПрограммеToolStripMenuItem1
+            // медицинскиеКартыToolStripMenuItem
             // 
-            this.оПрограммеToolStripMenuItem1.Image = global::Dentistry.Properties.Resources.About_50x50;
-            this.оПрограммеToolStripMenuItem1.Name = "оПрограммеToolStripMenuItem1";
-            this.оПрограммеToolStripMenuItem1.Size = new System.Drawing.Size(158, 22);
-            this.оПрограммеToolStripMenuItem1.Text = "О программе...";
-            this.оПрограммеToolStripMenuItem1.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
-            // 
-            // toolStripMain
-            // 
-            this.toolStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2});
-            this.toolStripMain.Location = new System.Drawing.Point(0, 24);
-            this.toolStripMain.Name = "toolStripMain";
-            this.toolStripMain.Size = new System.Drawing.Size(800, 25);
-            this.toolStripMain.TabIndex = 2;
-            this.toolStripMain.Text = "toolStrip1";
-            // 
-            // toolStripButton1
-            // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = global::Dentistry.Properties.Resources.Exit_50x501;
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
-            this.toolStripButton1.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
-            // 
-            // toolStripButton2
-            // 
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = global::Dentistry.Properties.Resources.About_50x50;
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "toolStripButton2";
-            this.toolStripButton2.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            this.медицинскиеКартыToolStripMenuItem.Image = global::Dentistry.Properties.Resources.MedCard_50x50;
+            this.медицинскиеКартыToolStripMenuItem.Name = "медицинскиеКартыToolStripMenuItem";
+            this.медицинскиеКартыToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.медицинскиеКартыToolStripMenuItem.Text = "Медицинские карты";
+            this.медицинскиеКартыToolStripMenuItem.Click += new System.EventHandler(this.медицинскиToolStripMenuItem_Click);
             // 
             // MainForm
             // 
@@ -229,6 +269,10 @@
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.ToolStripMenuItem медицинскиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справочникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem процедурыToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem врачиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem медицинскиеКартыToolStripMenuItem;
     }
 }
 
--- Dentristy/Dentristy/Properties/Resources.Designer.cs
+++ Dentristy/Dentristy/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace Dentistry.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap MedCard_50x50 {
+            get {
+                object obj = ResourceManager.GetObject("MedCard_50x50", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Procedures_50x50 {
             get {
                 object obj = ResourceManager.GetObject("Procedures_50x50", resourceCulture);
